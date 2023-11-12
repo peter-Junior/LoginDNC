753a7a7f2fa54ce45cac623f51c856c8eb660328
peterJunior <junior.pedroso234@gmail.com>
2023-11-12 20:46:59 -0300

    Button blocked to ensure form being filled out

--- validation.js
+++ validation.js
@@ -1,28 +1,18 @@
-const button = document.getElementById('btn01');
-button.addEventListener('click', changemodal);
+document.getElementById("btn01").disabled = true;
 
-console.log('im here!')
+document.getElementById("input02").addEventListener("input", () => {
+    
+    let input01 = document.getElementById("input01").value;
+    let input02 = document.getElementById("input02").value;
 
-function changemodal() {
-    let input01 = document.getElementById("input01");
-    let input02 = document.getElementById("input02");
-
-    const node = document.createElement('h1');
-    const textnode = document.createTextNode('Opsss...');
-    const node02 = document.createElement('p');
-    const textnode2 = document.createTextNode('Para validar o Login, precisamos que você preencha os campos do formulário. :-)');
-
-    if (input01.value == "" || input02.value == "") {
-        node.appendChild(textnode);
-        node02.appendChild(textnode2);
-        document.getElementsByClassName("modal-header").appendChild(node);
-        document.getElementsByClassName("modal-body").appendChild(node02)
-
-        /*document.getElementsByClassName("modal-title").innerHTML = 
-            'Opsss...';
-        document.getElementsByClassName("modal-body").innerHTML =
-            'Para validar o Login, precisamos que você preencha os campos do formulário. :-)';*/
-
-            console.log('Parece que pelo menos um dos campos está vazio!');
+    if (input01 !== null && input01 !== '' && input02 !== null && input02 !== '') {
+        document.getElementById("btn01").disabled = false;        
+    } else {
+        document.getElementById("btn01").disabled = true;
     }
-}
+});
+
+document.getElementById("btn01").addEventListener("click", () => {
+    document.getElementById("input01").value = '';
+    document.getElementById("input02").value = '';
+})
